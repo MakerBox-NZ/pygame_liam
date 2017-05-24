1a557047637b995f964bf40225671edf90801aac
ackerto983 <liam.george@hataitai.school.nz>
2017-05-24 17:09:57 +1200

    DA TUX RETURNS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! mama
    hell returns to da tuxes people ya people want damn things ain't no
    money da kill kadey jimkbos on da right position of gore no people who
    should kill us of striking foxy ladys and also lotta tickets to win a
    damn prize of killing da person that almost killed Emma Hellking
    AHHHHHHHHHHHHHHHHHHHHH!!!!!! this is my public service anounce of da tux
    and da world. thank ya yo tux for listening

---
+++ dice.py
@@ -0,0 +1,13 @@
+import random
+
+DASOPE= random.randint(1, 6)
+JIMINYTHEKILLAR= random.randint(1, 6)
+
+
+if DASOPE > JIMINYTHEKILLAR:
+    print ("JUST DOIN MY JOB, AND KILLING THE GUY DAT ALMOST KILLED EMMA!")
+else:
+    print ("I DON'T CARE ABOUT DAMN EMMA")
+        
+
+
