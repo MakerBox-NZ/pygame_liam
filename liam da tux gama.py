f120eb1168e26b12c7c0929d63f38f7d0107c08a
ackerto983 <liam.george@hataitai.school.nz>
2017-06-07 17:23:56 +1200

    added sprite

--- liam da tux gama.py
+++ liam da tux gama.py
@@ -8,6 +8,16 @@ import os
 '''OBJECTS'''
 # put classes & functions here
 
+class Player(pygame.sprite.Sprite):
+    #spawn a player
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(os.path.join('images', 'hero.png')).convert()
+        self.image.convert_alpha() #opimise for alpha
+        self.image.set_colorkey(alpha) #set alpha
+        self.rect = self.image.get_rect()
+        
+
 
 
 
@@ -15,7 +25,9 @@ import os
 # code runs once
 screenX = 960  #screen width
 screenY = 720  #screen height
-
+alpha = (0, 0, 0)
+black = (1, 1, 1)
+white = (255, 255, 255)
 fps = 40 #frame rate
 afps = 4 #animation cycles
 clock = pygame.time.Clock ( )
@@ -26,6 +38,15 @@ main = True
 screen = pygame.display.set_mode ( [screenX,  screenY] )
 backdrop = pygame.image.load (os.path.join ( 'images',   'stage.png' ) ) .convert ( )
 backdropRect = screen.get_rect ( )
+
+player = Player() #spawn player
+player.rect.x = 0 #go to x
+player.rect.y = 0 #go to y
+movingsprites = pygame.sprite.Group()
+movingsprites.add(player)
+
+
+
 
 
 
@@ -47,5 +68,11 @@ while main == True:
 
     screen.blit (backdrop,  backdropRect)
 
+
+    movingsprites.draw(screen) #draw player
     pygame.display.flip()
     clock.tick(fps)
+
+  
+
+
